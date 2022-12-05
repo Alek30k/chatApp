14a20d3fe6ae3800bc474b974c2579fe29b54795
Alek30k <alemank23k@gmail.com>
2022-12-05 12:15:19 -0300

    last commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://vivolabs.es/wp-content/uploads/2022/03/perfil-hombre-vivo.png"
+          alt=""
+        />
+
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://vivolabs.es/wp-content/uploads/2022/03/perfil-hombre-vivo.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
