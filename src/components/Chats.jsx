8e2c5990067e4d1d90a0ec58238d7b8e7c790138
Alek30k <alemank23k@gmail.com>
2022-12-09 21:19:03 -0300

    last commit

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { db } from "../firebase";
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+    };
+
+    return () => {
+      unsub();
+    };
+  }, [currentUser.uid]);
+
+  console.log(chats);
+
   return (
     <div className="chats">
       <div className="userChat">
